c00c4e68b16b4bb983f73b88e4df02b27e408604
Hernandiaz035 <hernandiaz035@gmail.com>
2020-05-09 10:42:49 +1000

    Board UI Generation

--- minesweeper.js
+++ minesweeper.js
@@ -4,6 +4,27 @@ function start() {
     
 }
 
+function getBoardUI(size){
+    var table = "";
+
+    table = `<table class="mx-auto">\n<tbody>\n`;
+
+    for (var i = 0; i < size; i++) {
+        table += `<tr class="cell-height">\n`;
+        for (var j = 0; j < size; j++) {
+            table += `<td class="m-0 p-0 b-0 cell-height cell-width">
+                <button class="m-0 p-0 cell-height cell-width" id="cell-${i}-${j}" onclick="cellClick('cell-${i}-${j}')">
+                </button>
+                </td>\n`;
+        }
+        table += `</tr>\n`;
+    }
+
+	table += `</tbody>\n</table>`;
+	
+	return table;
+}
+
 function generateMatrix(size, quantity) {
 	var board = new Array(size).fill();
 	for (var i = board.length - 1; i >= 0; i--) {
